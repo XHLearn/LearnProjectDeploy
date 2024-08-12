5371826d671714d400a7be9f74b61c16e8d66e37
xiaohao <hao.xiao@narwal.com>
2024-08-12 22:14:03 +0800

    增加loguru

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 import uvicorn
-
 from fastapi import FastAPI
+from loguru import logger
 
 app = FastAPI()
+
+logger.info("run app")
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/add")
+async def add():
+    data = {"a": 1}
+    try:
+        data = {"a": a}
+    except Exception as e:
+        logger.exception(e)
+    return data
 
 
 if __name__ == "__main__":
